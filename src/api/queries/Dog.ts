37da4cb6782d6ef6d4a3ddfabfc1186dc0cddfef
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-28 00:25:44 -0300

    feat: add graphql get dogs query

---
+++ src/api/queries/Dog.ts
@@ -0,0 +1,18 @@
+import gql from "graphql-tag"
+
+export const DogFragment = gql`
+  fragment DogFields on Dog {
+    id
+    name
+    age
+    isPedigree
+  }
+`
+
+export const GET_DOGS = gql`
+  query {
+    dogs {
+      ...DogFields
+    }
+  }
+`
