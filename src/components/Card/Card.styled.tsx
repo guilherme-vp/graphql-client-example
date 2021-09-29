255fe7bacd9d9d815a62498501bfec71f28fd8d9
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-28 21:14:56 -0300

    feat: create dog card styles

--- src/components/Card/Card.styled.tsx
+++ src/components/Card/Card.styled.tsx
@@ -1,10 +1,32 @@
 import styled from 'styled-components'
 
 export const CardContainer = styled.div`
+	width: 200px;
+	padding: 24px;
+	margin: 8px 16px;
+	word-wrap: break-word;
+	background-color: #f2f2f2;
+	box-shadow: 0 2px 3px #ccc;
+	border-radius: 8px;
+	color: #333;
+	p {
+		margin: 4px 0;
+	}
+`
+
+export const DeleteButton = styled.button`
+	margin-top: 12px;
+	background-color: #d11a2a;
 	width: 100%;
-	height: 56px;
-	border-radius: 4px;
-	position: relative;
-	background-color: rgba(255, 255, 255, 0.3);
-	transition: 0.3s all;
+	border: 0 none;
+	font-weight: 600;
+	padding: 6px 16px;
+	border-radius: 12px;
+	color: #fff;
+	cursor: pointer;
+	transition: background-color 0.2s;
+
+	&:hover {
+		background-color: #aa101d;
+	}
 `
