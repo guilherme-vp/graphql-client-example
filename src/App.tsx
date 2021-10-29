55c6006b777f16418630ac7edd417d4f2a8873c7
Guilherme Vieira <guivpw68@gmail.com>
2021-10-29 01:31:08 -0300

    refactor: change function mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
 import { ApolloProvider } from '@apollo/client'
 import { GlobalStyle } from './styles/global'
 import { client } from './api/client'
 import { Dogs } from './screens/Dogs'
 
-function App() {
-	return (
-		<ApolloProvider client={client}>
-			<GlobalStyle />
-			<Dogs />
-		</ApolloProvider>
-	)
-}
+export const App = () => (
+	<ApolloProvider client={client}>
+		<GlobalStyle />
+		<Dogs />
+	</ApolloProvider>
+)
 
 export default App
